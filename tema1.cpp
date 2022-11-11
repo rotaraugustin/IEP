68c191feffe952f5d89b093c34758ae61cf2a840
rotaraugustin <catalin.rotar@student.upt.ro>
2022-11-11 13:59:42 +0200

    inheretence add

--- tema1.cpp
+++ tema1.cpp
@@ -34,15 +34,26 @@ void cladire::cameraRatio()
     ratio=(float)dimensiune/nrCamere;
     cout<<"cladire ratio="<<ratio<<"\n";
 }
+class casa:public cladire
+{
+public:
+    casa(){cout << "casa initializata.\n";}
+   ~casa(){cout << "casa distrusa.\n";}
+    casa(int n,int d,int g):cladire(3,100,3){}
+};
 int main()
 {
      cladire c1,c2;
+     casa cs,cs1;
+     // cs=cs1;
+     // casa cs2(cs);
      // c1=c2;
      // cladire c3(c1);
-     cladire c1,c2;
+     cs.setCladire(3,100,3);
      c1.setCladire(6,250,5);
      c2.setCladire(5,200,4);
      c1.cameraRatio();
      c2.cameraRatio();
+     cs.cameraRatio();
     return 0;
 }
