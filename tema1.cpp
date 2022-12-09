ba8b3147a8473078e9dfa77a361e2365f97d457c
rotaraugustin <catalin.rotar@student.upt.ro>
2022-12-09 17:02:04 +0200

    tema3

--- tema1.cpp
+++ tema1.cpp
@@ -1,109 +1,78 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-void message(char *s)
-{
-    cout<<s<<"\n";
-}
-class usa{
-int nrUsi;
+#include<thread>
+#include <mutex>
+class Lock{
 public:
-int getUsi()
-{
-    return nrUsi;
-}
-void setUsi(int u)
-{
-    nrUsi=u;
-}
+explicit Lock(mutex *pm)
+: mutexPtr(pm)
+{ mutexPtr->lock(); cout<<"Lock mutex\n";} // acquire resource
+~Lock() { mutexPtr->unlock(); cout<<"Unlock mutex\n"; } // release resource
+private:
+Lock(const Lock& t);
+Lock& operator=(const Lock& t);
+mutex *mutexPtr;
 };
 class cladire {
-int nrCamere,dimensiune,geamuri;//dimensiune m^2
-usa * usi;
-public:
-cladire& operator=(const cladire& c)
-{
-    message("cladire copy assignment");
-    if (this == &c) return *this;
-    nrCamere=c.nrCamere;
-    dimensiune=c.dimensiune;
-    geamuri=c.geamuri;
-    delete usi;
-    usi=new usa(*c.usi);
-    return *this;
-}
-cladire(const cladire& c){
-  message("cladire copy constructor");
-  nrCamere=c.nrCamere;
-  dimensiune=c.dimensiune;
-  geamuri=c.geamuri;
-  usi=c.usi;
-}
-cladire(); // constructor
-~cladire(); // destructor
-void setCladire(int n,int d,int g,int u);
-void cameraRatio();
-void cladireUsi()
-{
-    cout<<usi->getUsi()<<"\n";
-}
+int nrCamere,geamuri,dimensiune;
+   public:
+      static cladire *make_cladire(int choice);
+      cladire() {
+         nrCamere=0;
+         geamuri=0;
+         dimensiune=0;
+         cout<<"Constructing cladire\n ";
+      }
+     virtual ~cladire() {
+         cout<<"Destructing cladire\n";
+      }
+      virtual void getObject()
+      {
+         cout<<"Sunt cladire"<<"\n";
+      }
+};
+class casa: public cladire {
+   public:
+      casa() {
+         cout<<"Constructing casa\n";
+      }
+      ~casa() {
+         cout<<"Destructing casa\n";
+      }
+        void getObject()
+      {
+         cout<<"Sunt casa"<<"\n";
+      }
 };
 
-cladire::cladire()
-{
-nrCamere= dimensiune =geamuri= 0;
-usi=new usa();
-cout << "cladire initializata.\n";
-}
-
-cladire::~cladire()
-{
-cout << "cladire distrusa.\n";
-}
-void cladire::setCladire(int n,int d,int g,int u)
-{
-    nrCamere=n; dimensiune=d; geamuri=g;
-    usi->setUsi(u);
-}
-void cladire::cameraRatio()
-{
-    float ratio;
-    ratio=(float)dimensiune/nrCamere;
-    cout<<"cladire ratio="<<ratio<<"\n";
-}
-class casa:public cladire
-{
+class apartament:public cladire{
 public:
-   casa(){cout << "casa initializata.\n";}
-   ~casa(){cout << "casa distrusa.\n";}
-   casa(const casa &cs):cladire(cs){
-    message("casa copy constructor");
-   }
-   casa& operator=(const casa &cs)
-   {
-    message("casa copy assignment");
-    cladire::operator=(cs);
-    return *this;
-   }
+      apartament() {
+         cout<<"Constructing apartament\n";
+      }
+      ~apartament() {
+         cout<<"Destructing apartament\n";
+      }
+        void getObject()
+      {
+         cout<<"Sunt apartament"<<"\n";
+      }
 };
-int main()
+int main() {
+   int choice=1;
+   mutex m;
+   Lock ml(&m);
+   // Lock ml2(ml1);
+   std::shared_ptr<cladire>c(cladire::make_cladire(choice));
+   c->getObject();
+   return 0;
+}
+cladire*cladire::make_cladire(int choice)
 {
-     cladire c1,c2,c3;
-     casa cs1,cs3;
-     cs1.setCladire(3,100,4,5);
-     c1.setCladire(6,250,5,9);
-     c1=c1;
-     cladire c4(c1);
-     casa cs2(cs1);
-     cs3=cs1;
-     c2=c3=c1;
-     c1.cameraRatio();
-     c2.cameraRatio();
-     c4.cameraRatio();
-     c1.cladireUsi();
-     c2.cladireUsi();
-     c3.cladireUsi();
-     c4.cladireUsi();
-     cs2.cladireUsi();
-     cs3.cladireUsi();
-    return 0;
+  if (choice == 1)
+    return new casa;
+  else if (choice == 2)
+    return new apartament;
+  else
+    return NULL;
 }
